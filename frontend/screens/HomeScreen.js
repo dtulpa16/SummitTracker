13427f1a89df9946c6b6ba6d1d78a2653d2c3483
Megan Cindric <megancindric@gmail.com>
2023-05-11 10:27:56 -0600

    📦 NEW: Added example navigation buttons

--- frontend/screens/HomeScreen.js
+++ frontend/screens/HomeScreen.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
-export default function HomeScreen() {
+import { Text, View, Button } from "react-native";
+export default function HomeScreen({ navigation }) {
   const [hikes, setAllHikes] = useState([]);
 
   useEffect(() => {
@@ -22,6 +22,10 @@ export default function HomeScreen() {
     <View className="flex-1 items-center justify-center bg-emerald-900">
       <Text className=" text-white text-5xl">SummitTracker!</Text>
       {/* StatusBar displays base phone info*/}
+      <Button
+        title="Past Hikes"
+        onPress={() => navigation.navigate("Past Hikes")}
+      />
       <StatusBar style="auto" />
     </View>
   );
--- frontend/screens/PastHikesScreen.js
+++ frontend/screens/PastHikesScreen.js
@@ -1,9 +1,10 @@
-import { View,Text,StatusBar } from "react-native";
+import { View, Text, StatusBar, Button } from "react-native";
 
-const PastHikesScreen = () => {
+const PastHikesScreen = ({ navigation }) => {
   return (
     <View className="flex-1 items-center justify-center bg-emerald-900">
       <Text className=" text-white text-5xl">SummitTracker</Text>
+      <Button title="Home!" onPress={() => navigation.navigate("Home")} />
       <StatusBar style="auto" />
     </View>
   );
