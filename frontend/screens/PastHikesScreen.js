c010a80d809285133a8ac5e6c379a993a4e04a5f
Megan Cindric <megancindric@gmail.com>
2023-05-12 09:12:23 -0600

    👌 IMPROVE: Refactored HikeDetailsScreen to grab ID using route parameters

--- frontend/screens/PastHikesScreen.js
+++ frontend/screens/PastHikesScreen.js
@@ -5,15 +5,28 @@ const PastHikesScreen = ({ navigation }) => {
   const { data, isLoading, error } = useFetch(`${URL_HOST}/api/summit/`);
   return !isLoading ? (
     <View className="flex-1 items-center justify-center bg-emerald-900">
-      <Text className=" text-white text-5xl">SummitTracker</Text>
+      <Text className=" text-white text-5xl">Past Hikes</Text>
       <View className="flex flex-col gap-5">
-        {data.map((hike) => (
-          <View className="flex border p-4 rounded-lg drop-shadow-md">
-            <Text className=" text-2xl text-white">{hike.name}</Text>
-            <Text className=" text-lg text-white">{hike.altitude}</Text>
-            <Text className=" text-xl text-white">{hike.length} miles</Text>
-          </View>
-        ))}
+        {data.length > 0 ? (
+          data.map((hike, index) => (
+            <View
+              className="flex border p-4 rounded-lg drop-shadow-md"
+              key={index}
+            >
+              <Text className=" text-2xl text-white">{hike.name}</Text>
+              <Text className=" text-lg text-white">{hike.altitude}</Text>
+              <Text className=" text-xl text-white">{hike.length} miles</Text>
+              <Button
+                onPress={() =>
+                  navigation.navigate("Hike Details", { hikeId: hike._id })
+                }
+                title="Details"
+              ></Button>
+            </View>
+          ))
+        ) : (
+          <Text>No recorded hikes!</Text>
+        )}
       </View>
       <Button title="Home!" onPress={() => navigation.navigate("Home")} />
       <StatusBar style="auto" />
