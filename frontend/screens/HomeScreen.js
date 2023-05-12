0496aaecfcd7a9e152a53ebc47a40be6731d769b
Megan Cindric <megancindric@gmail.com>
2023-05-12 09:04:52 -0600

    👌 IMPROVE: Moved getAllHikes to PaskHikesScreen, refactored to use useFetch

--- frontend/screens/HomeScreen.js
+++ frontend/screens/HomeScreen.js
@@ -1,23 +1,6 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { Text, View, Button } from "react-native";
-import { URL_HOST } from "../utils/urlHost";
 export default function HomeScreen({ navigation }) {
-  const [hikes, setAllHikes] = useState([]);
-
-  useEffect(() => {
-    getAllHikes();
-  }, []);
-
-  const getAllHikes = async () => {
-    try {
-      const response = await axios.get(`${URL_HOST}/api/summit/`);
-      console.log(response.data);
-    } catch (error) {
-      console.log("getAllHikes error: ", error);
-    }
-  };
   return (
     <View className="flex-1 items-center justify-center bg-emerald-900">
       <Text className=" text-white text-5xl">SummitTracker!</Text>
--- frontend/screens/PastHikesScreen.js
+++ frontend/screens/PastHikesScreen.js
@@ -1,12 +1,25 @@
 import { View, Text, StatusBar, Button } from "react-native";
-
+import { URL_HOST } from "../utils/urlHost";
+import useFetch from "../hooks/useFetch";
 const PastHikesScreen = ({ navigation }) => {
-  return (
+  const { data, isLoading, error } = useFetch(`${URL_HOST}/api/summit/`);
+  return !isLoading ? (
     <View className="flex-1 items-center justify-center bg-emerald-900">
       <Text className=" text-white text-5xl">SummitTracker</Text>
+      <View className="flex flex-col gap-5">
+        {data.map((hike) => (
+          <View className="flex border p-4 rounded-lg drop-shadow-md">
+            <Text className=" text-2xl text-white">{hike.name}</Text>
+            <Text className=" text-lg text-white">{hike.altitude}</Text>
+            <Text className=" text-xl text-white">{hike.length} miles</Text>
+          </View>
+        ))}
+      </View>
       <Button title="Home!" onPress={() => navigation.navigate("Home")} />
       <StatusBar style="auto" />
     </View>
+  ) : (
+    <Text>Loading...</Text>
   );
 };
 
