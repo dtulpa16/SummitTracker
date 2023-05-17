e2a5d32783d78ee92e06a2ea2e4969565d6ba342
Megan Cindric <megancindric@gmail.com>
2023-05-17 12:48:39 -0600

    👌 IMPROVE: Added explicit H/W for map component

--- frontend/components/MapComponent.js
+++ frontend/components/MapComponent.js
@@ -1,16 +1,16 @@
 import React from "react";
 import MapView, { Marker } from "react-native-maps";
-import {Text } from "react-native";
+import { Text, View } from "react-native";
 export default function MapComponent({ latitude, longitude }) {
-    console.log("LAT: ", latitude, "\nLONG: ", longitude)
+  console.log("LAT: ", latitude, "\nLONG: ", longitude);
   return latitude && longitude ? (
     <MapView
-      style={{ flex: 1 }}
+      className="flex w-4/5 h-1/3"
       initialRegion={{
         latitude: "39.5501",
         longitude: "-107.5808",
-        latitudeDelta: 10,
-        longitudeDelta: 10,
+        latitudeDelta: 5,
+        longitudeDelta: 5,
       }}
     >
       <Marker
