1c01473236f625ff59146c2dbfe38ec1fba1ee30
Megan Cindric <megancindric@gmail.com>
2023-05-17 12:18:55 -0600

    👌 IMPROVE: Adding device-specific IP config to urlHost

--- frontend/utils/urlHost.js
+++ frontend/utils/urlHost.js
@@ -2,4 +2,5 @@ export const URL_HOST =
   process.env.NODE_ENV === "production"
     ? "PRODUCTION URL HERE"
     : //Change as needed w/ new ngrok hosts
+      //Megan "http://100.64.58.37:5000"
       "http://100.64.58.11:5000";
