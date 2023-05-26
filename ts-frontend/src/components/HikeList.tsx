a50d0fb5ea461bc9cbd3a56fdfc8254d9c453c52
Daniel Tulpa <dtulpa16@yahoo.com>
2023-05-26 10:46:05 -0600

    Added Hero Icons Next To Hike Card Properties

--- ts-frontend/src/components/HikeList.tsx
+++ ts-frontend/src/components/HikeList.tsx
@@ -1,5 +1,10 @@
 import React, { useState, useEffect, FC } from "react";
 import { Hike } from "../interfaces/Hike";
+import {
+  CalendarIcon,
+  ArrowCircleUpIcon,
+  LocationMarkerIcon,
+} from "@heroicons/react/solid";
 import { Image } from "../interfaces/Image";
 import axios from "axios";
 export default function HikeList() {
@@ -25,10 +30,13 @@ interface HikeMapperProps {
 
 const HikeMapper: FC<HikeMapperProps> = ({ hikes }) => {
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-2 md:w-1/3">
       {hikes.length ? (
         hikes.map((hike, index) => (
-          <div key={index} >
+          <div
+            key={index}
+            className="bg-blue-500 text-white p-4 rounded shadow-lg"
+          >
             <HikeCard hike={hike} />
           </div>
         ))
@@ -66,13 +74,23 @@ const HikeCard: FC<HikeProp> = ({ hike }) => {
     }, 5000);
     return () => clearInterval(interval);
   }, [image]);
+
   return image ? (
-    <div className="flex flex-row justify-between md:w-1/3">
-      <div className="flex flex-col gap-1">
-        <h2>{hike.name}</h2>
-        <h3>{hike.date.split("T")[0]}</h3>
-        <h3>{hike.altitude}ft Elevation</h3>
-        <h3>{hike.length} Miles</h3>
+    <div className="flex flex-row justify-between">
+      <div className="flex flex-col gap-1 text-white">
+        <h2 className="font-bold text-xl">{hike.name}</h2>
+        <h3 className="flex items-center gap-2">
+          <CalendarIcon className="h-5 w-5" />
+          {hike.date.split("T")[0]}
+        </h3>
+        <h3 className="flex items-center gap-2">
+          <ArrowCircleUpIcon className="h-5 w-5" />
+          {hike.altitude}ft Elevation
+        </h3>
+        <h3 className="flex items-center gap-2">
+          <LocationMarkerIcon className="h-5 w-5" />
+          {hike.length} Miles
+        </h3>
       </div>
       <div className="h-[250px] w-[250px] bg-slate-400">
         <img
