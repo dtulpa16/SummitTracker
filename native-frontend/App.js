aefc63bdc6d9dae33be35ead8602fba761674b14
Megan Cindric <megancindric@gmail.com>
2023-06-07 09:39:42 -0600

    👌 IMPROVE: Updating status bar title, making past hike cards clickable

--- native-frontend/App.js
+++ native-frontend/App.js
@@ -12,7 +12,18 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#172554",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+            color: "white",
+          },
+        }}
+      >
         <Stack.Screen
           name="Home"
           component={HomeScreen}
--- native-frontend/screens/PastHikesScreen.js
+++ native-frontend/screens/PastHikesScreen.js
@@ -5,31 +5,28 @@ const PastHikesScreen = ({ navigation }) => {
   const { data, isLoading, error } = useFetch(`${URL_HOST}/api/summit/`);
   return !isLoading && data ? (
     <View className="flex-1 items-center justify-center bg-blue-950">
-      <Text className=" text-orange-50 text-5xl">Past Hikes</Text>
-      <View className="flex flex-col gap-5 text-orange-50">
+      <Text className=" text-orange-50 text-5xl pb-5">Past Hikes</Text>
+      <View className="flex flex-col gap-6 text-orange-50">
         {data.length > 0 ? (
           data.map((hike, index) => (
-            <View
-              className="flex border p-4 rounded-lg drop-shadow-md border-amber-50"
+            <TouchableOpacity
+              className="flex border p-4 rounded-lg drop-shadow-md border-amber-50 min-h-[150] min-w-[200]"
               key={index}
+              onPress={() =>
+                navigation.navigate("Hike Details", {
+                  hikeId: hike?._id,
+                  name: hike?.name,
+                })
+              }
             >
-              <Text className=" text-2xl text-orange-50">{hike?.name}</Text>
-              <Text className=" text-lg text-orange-50">{hike?.altitude}</Text>
+              <Text className=" text-3xl text-orange-50">{hike?.name}</Text>
+              <Text className=" text-xl text-orange-50">
+                {hike?.altitude} ft. elevation
+              </Text>
               <Text className=" text-xl text-orange-50">
                 {hike?.length} miles
               </Text>
-              <TouchableOpacity
-                onPress={() =>
-                  navigation.navigate("Hike Details", {
-                    hikeId: hike?._id,
-                    name: hike?.name,
-                  })
-                }
-                className="flex justify-center p-4 bg-orange-100 rounded-lg drop-shadow-md m-auto"
-              >
-                <Text className="text-blue-950 text-md">Details</Text>
-              </TouchableOpacity>
-            </View>
+            </TouchableOpacity>
           ))
         ) : (
           <Text>No recorded hikes!</Text>
@@ -37,9 +34,9 @@ const PastHikesScreen = ({ navigation }) => {
       </View>
       <TouchableOpacity
         onPress={() => navigation.navigate("Home")}
-        className="flex justify-center p-4 bg-orange-100 rounded-lg"
+        className="flex justify-center p-4 bg-orange-100 rounded-lg mt-5"
       >
-        <Text className="text-blue-950 text-xl">Return Home</Text>
+        <Text className="text-blue-950 text-xl font-bold">Return Home</Text>
       </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
