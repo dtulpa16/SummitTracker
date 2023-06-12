86702a9dac189d9acc507fe947fa0a38df83357d
Daniel Tulpa <dtulpa16@yahoo.com>
2023-06-12 07:44:24 -0600

    Fixed Bug With New Note Modal

--- ts-frontend/src/components/NewNoteModal.tsx
+++ ts-frontend/src/components/NewNoteModal.tsx
@@ -47,13 +47,13 @@ const NewNoteModal: React.FC<ThemeProps & ModalProps> = ({
 
   return isOpen ? (
     <div
-      className={`${
-        theme === "dark" ? "md:bg-gray-600 bg-gray-900 text-white" : "bg-white"
-      } fixed inset-0 flex items-center justify-center z-40 bg-opacity-80 md:bg-opacity-60`}
+    className={`${
+      theme === "dark" ? "md:bg-gray-600 bg-gray-900 text-white" : "bg-blue-300"
+    } fixed inset-0 flex items-center justify-center z-40 bg-opacity-80 md:bg-opacity-60`}
     >
       <div
         className={`${
-          theme === "dark" ? "bg-gray-600 text-white" : "bg-white"
+          theme === "dark" ? "bg-gray-600 text-white" : "bg-white text-black"
         } p-8 md:p-12 rounded shadow-md z-50 md:w-auto w-11/12`}
       >
         <h1 className="font-semibold text-xl mb-2">Add Note</h1>
