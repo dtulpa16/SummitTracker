1c2dc879f8b79248f7b57bf71bde2f1779389d9c
Megan Cindric <megancindric@gmail.com>
2023-06-19 16:07:57 -0600

    👌 IMPROVE: Added individual loading spinners for each hike image

--- native-frontend/components/HikeImages.js
+++ native-frontend/components/HikeImages.js
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, Image, FlatList, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  FlatList,
+  ScrollView,
+  ActivityIndicator,
+} from "react-native";
 import useFetch from "../hooks/useFetch";
 import { URL_HOST } from "../utils/urlHost";
 import LoadingIcon from "../assets/loading.svg";
@@ -18,18 +25,26 @@ export default function HikeImages({ hikeId }) {
     </View>
   ) : (
     <View className="flex h-full w-full justify-center items-center">
-      <LoadingIcon width={100} height={100} className="animate-spin" />
+      {/* <LoadingIcon width={100} height={100} className="animate-spin" /> */}
+      <ActivityIndicator size="large" color="#fdbb74" />
     </View>
   );
 }
 
 export const ImageCard = ({ image }) => {
-  console.log(`IMAGE DATA: ${URL_HOST}${image?.imageUrl}`);
+  // console.log(`IMAGE DATA: ${URL_HOST}${image?.imageUrl}`);
+  const [isLoading, setIsLoading] = useState(true);
   return (
-    <View className=" justify-center items-center">
+    <View className="relative w-[195px] h-[195px] justify-center items-center">
+      {isLoading && (
+        <View className="absolute inset-0 flex items-center justify-center">
+          <ActivityIndicator size="large" color="#fdbb74" />
+        </View>
+      )}
       <Image
         source={{ uri: `${URL_HOST}${image?.imageUrl}` }}
-        className="w-[195px] h-[195px]"
+        className="absolute w-[195px] h-[195px]"
+        onLoadEnd={() => setIsLoading(false)}
         // style={{ width: "100%", height: "100%" }}
         resizeMode="contain"
       />
