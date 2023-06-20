58d78ddada7fb87fd685aaeacbc3c079ef98010e
Megan Cindric <megancindric@gmail.com>
2023-06-20 10:19:05 -0600

    📦 NEW: Adding fetch for total length and altitude

--- native-frontend/screens/HomeScreen.js
+++ native-frontend/screens/HomeScreen.js
@@ -2,28 +2,28 @@ import { StatusBar } from "expo-status-bar";
 import { Text, View, Image, Button, TouchableOpacity } from "react-native";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import useFetch from "../hooks/useFetch";
+import { URL_HOST } from "../utils/urlHost";
+import StatDisplay from "../components/StatDisplay";
 export default function HomeScreen({ navigation }) {
   const [totalStats, setTotalStats] = useState(null);
-
-  useEffect(() => {
-    const getTotalStats = async () => {
-      try {
-        let response = await axios.get(
-          "http://100.64.58.37:5000/api/summit/total/altitude"
-        );
-        setTotalStats(response.data);
-      } catch (error) {
-        console.log("Error in getTotalStats: ", error);
-      }
-    };
-    getTotalStats();
-  }, []);
+  const { data, isLoading, error } = useFetch(
+    `${URL_HOST}/api/summit/total/altitude`
+  );
 
   return (
     <View className="flex-1 items-center bg-blue-950 justify-around">
       <View className="flex items-center gap-5">
         <Image source={require("../assets/icon.png")} className=" h-36 w-36" />
         <Text className="text-orange-50 text-5xl">SummitTracker</Text>
+      </View>
+      <View>
+        <Text className="text-xl font-bold text-white">
+          {data?.altitude ? data.altitude : null} TEST
+        </Text>
+        <Text className="text-xl font-bold text-white">
+          {data?.length ? data.length : null} TEST
+        </Text>
       </View>
       <View className="flex flex-row gap-10">
         <TouchableOpacity
