0cd7a75d49abc9f6b31641742cfc3a6905bfb4de
Megan Cindric <megancindric@gmail.com>
2023-06-28 15:18:31 -0600

    👌 IMPROVE: Replacing home button in navbar with icon

--- native-frontend/App.js
+++ native-frontend/App.js
@@ -13,7 +13,12 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerBackTitleVisible: false,
+          headerBackVisible: false,
+        }}
+      >
         <Stack.Screen
           name="Home"
           component={HomeScreen}
--- native-frontend/components/NavBar.js
+++ native-frontend/components/NavBar.js
@@ -1,12 +1,14 @@
 import { View, Text, TouchableOpacity } from "react-native";
+import HomeIcon from "../assets/homeIcon.svg";
+
 const NavBar = ({ navigation }) => {
   return (
-    <View className="flex flex-row content-around justify-around">
+    <View className="flex flex-row justify-around items-center w-full">
       <TouchableOpacity onPress={() => navigation.navigate("Home")}>
-        <Text>Home</Text>
+        <HomeIcon width={38} height={38} />
       </TouchableOpacity>
       <TouchableOpacity onPress={() => navigation.navigate("Past Hikes")}>
-        <Text>Past Hikes</Text>
+        <Text className="text-blue-950 text-xl font-bold">Past Hikes</Text>
       </TouchableOpacity>
       {/* <TouchableOpacity onPress={()=>navigation.navigate("Home")}>Add Hike</TouchableOpacity> */}
     </View>
