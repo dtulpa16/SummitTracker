0dca2d949c12d37eabf41b84973e05ffff875e26
Daniel Tulpa <dtulpa16@yahoo.com>
2023-07-12 09:39:06 -0600

    Added Base Of Login Modal

---
+++ ts-frontend/src/components/LoginModal.tsx
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+
+type ThemeProps = {
+  theme: "light" | "dark";
+};
+interface ModalProps {
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const LoginModal: React.FC<ThemeProps & ModalProps> = ({
+  theme,
+  isOpen,
+  setIsOpen,
+
+}) => {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+
+    setIsOpen(false);
+  };
+
+  return isOpen ? (
+    <div
+    className={`${
+      theme === "dark" ? "md:bg-gray-900 bg-gray-900 text-white" : "bg-gray-900 text-black"
+    } fixed inset-0 flex items-center justify-center z-50 bg-opacity-80 md:bg-opacity-60`}
+    >
+      <div
+        className={`${
+          theme === "dark" ? "bg-gray-600 text-white" : "bg-white text-black"
+        } p-8 md:p-12 rounded shadow-md z-50 md:w-auto w-11/12`}
+      >
+        <h1 className="font-semibold text-xl mb-2">Sign In</h1>
+        <form onSubmit={handleSubmit} className="flex flex-col">
+          <label>Username</label>
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Username"
+            className={`mb-4 md:w-[450px] border rounded p-2 ${
+              theme === "dark" ? "bg-gray-500 text-white" : "bg-gray-200"
+            }`}
+          />
+          <label>Password</label>
+          <input
+            type="text"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            className={`mb-4 md:w-[450px] border rounded p-2 ${
+              theme === "dark" ? "bg-gray-500 text-white" : "bg-gray-200"
+            }`}
+          />
+
+          <div className="flex flex-row justify-between">
+            <button
+              type="submit"
+              className={`${
+                theme === "dark"
+                  ? " bg-gray-500 text-white"
+                  : "bg-blue-500 text-white"
+              } mt-4 px-4 py-2 rounded shadow font-semibold`}
+            >
+              Log In
+            </button>
+            <button
+              type="button"
+              onClick={() => {
+                setIsOpen(false);
+              }}
+              className={`${
+                theme === "dark"
+                  ? " bg-gray-500 text-white"
+                  : "bg-blue-500 text-white"
+              } mt-4 px-4 py-2 rounded shadow font-semibold`}
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  ) : null;
+};
+export default LoginModal;
--- ts-frontend/src/components/Navbar.tsx
+++ ts-frontend/src/components/Navbar.tsx
@@ -1,42 +1,54 @@
 import { useState } from "react";
-import { SunIcon, MoonIcon } from "@heroicons/react/solid";
+import { SunIcon, MoonIcon, LoginIcon } from "@heroicons/react/solid";
 import MountainIcon from "./MountainIcon";
 import { useTheme } from "../context/ThemeContext";
+import LoginModal from "./LoginModal";
 interface NavbarProps {}
 
 const Navbar: React.FC<NavbarProps> = () => {
   const [theme, themeToggler] = useTheme();
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav
-      className={`p-4 ${
-        theme === "dark" ? "bg-gray-800" : "bg-blue-500"
-      } text-white`}
-    >
-      <div className="max-w-7xl mx-auto md:px-2 sm:px-6 lg:px-8">
-        <div className="relative flex items-center justify-between h-16">
-          <div className="flex items-center px-1 lg:px-0">
-            <MountainIcon />
-            <div className="ml-1 md:text-2xl text-lg font-semibold">
-              Summit Tracker
+    <>
+      <nav
+        className={`p-4 ${
+          theme === "dark" ? "bg-gray-800" : "bg-blue-500"
+        } text-white`}
+      >
+        <div className="max-w-7xl mx-auto md:px-2 sm:px-6 lg:px-8">
+          <div className="relative flex items-center justify-between h-16">
+            <div className="flex items-center px-1 lg:px-0">
+              <MountainIcon />
+              <div className="ml-1 md:text-2xl text-lg font-semibold">
+                Summit Tracker
+              </div>
+            </div>
+            <div className="lg:ml-4 lg:flex lg:items-center">
+              <button
+                className="p-1 border-2 border-transparent text-white rounded-full hover:text-gray-500 focus:outline-none focus:text-gray-500 focus:bg-gray-100 transition duration-150 ease-in-out mr-3"
+                aria-label="Login"
+                onClick={() => setIsOpen(true)}
+              >
+                <LoginIcon className="h-6 w-6" aria-hidden="true" />
+              </button>
+              <button
+                className="p-1 border-2 border-transparent text-white rounded-full hover:text-gray-500 focus:outline-none focus:text-gray-500 focus:bg-gray-100 transition duration-150 ease-in-out"
+                aria-label="Toggle dark mode"
+                onClick={themeToggler}
+              >
+                {theme === "dark" ? (
+                  <SunIcon className="h-6 w-6" aria-hidden="true" />
+                ) : (
+                  <MoonIcon className="h-6 w-6" aria-hidden="true" />
+                )}
+              </button>
             </div>
           </div>
-          <div className="lg:ml-4 lg:flex lg:items-center">
-            <button
-              className="p-1 border-2 border-transparent text-white rounded-full hover:text-gray-500 focus:outline-none focus:text-gray-500 focus:bg-gray-100 transition duration-150 ease-in-out"
-              aria-label="Toggle dark mode"
-              onClick={themeToggler}
-            >
-              {theme === "dark" ? (
-                <SunIcon className="h-6 w-6" aria-hidden="true" />
-              ) : (
-                <MoonIcon className="h-6 w-6" aria-hidden="true" />
-              )}
-            </button>
-          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      <LoginModal theme={theme} isOpen={isOpen} setIsOpen={setIsOpen}/>
+    </>
   );
 };
 
