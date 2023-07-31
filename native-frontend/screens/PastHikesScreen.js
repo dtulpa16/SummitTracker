6c9d3564ffa9454165d5d90439309040275aad9a
Megan Cindric <megancindric@gmail.com>
2023-07-31 15:41:54 -0600

    👌 IMPROVE: Changing past hike view toggle to a switch

--- native-frontend/screens/PastHikesScreen.js
+++ native-frontend/screens/PastHikesScreen.js
@@ -2,50 +2,46 @@ import {
   View,
   Text,
   StatusBar,
-  Button,
-  TouchableOpacity,
   SafeAreaView,
   ScrollView,
+  ActivityIndicator,
+  Switch,
 } from "react-native";
 import { URL_HOST } from "../utils/urlHost";
 import useFetch from "../hooks/useFetch";
-import CustomButton from "../components/elements/CustomButton";
-import { ActivityIndicator } from "react-native";
 import { useState } from "react";
 import PastHikeList from "../components/PastHikeList";
 import AllHikesMap from "../components/AllHikesMap";
 const PastHikesScreen = ({ navigation }) => {
   const { data, isLoading, error } = useFetch(`${URL_HOST}/api/summit/`);
-  const [currentView, setCurrentView] = useState("list");
+  const [showMap, setShowMap] = useState(false);
+  const toggleSwitch = () => setShowMap((previousState) => !previousState);
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-blue-950">
       {!isLoading ? (
         <>
           <Text className=" text-white text-5xl py-4">Past Hikes</Text>
           <View className="flex flex-row justify-around">
-            <CustomButton
-              onPress={() => setCurrentView("list")}
-              text="List View"
-              customTextStyle="text-blue-950 text-lg font-bold"
-              customButtonStyle="flex justify-center p-4 bg-orange-200 rounded-xl border-2 border-blue-950 ml-2"
+            <Text>List</Text>
+            <Switch
+              trackColor={{ false: "#767577", true: "#81b0ff" }}
+              thumbColor={showMap ? "#ffb74d" : "#f4f3f4"}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={toggleSwitch}
+              value={showMap}
             />
-            <CustomButton
-              onPress={() => setCurrentView("map")}
-              text="Map View"
-              customTextStyle="text-blue-950 text-lg font-bold"
-              customButtonStyle="flex justify-center p-4 bg-orange-200 rounded-xl border-2 border-blue-950 ml-2"
-            />
+            <Text>Map</Text>
           </View>
           <View className="flex-1">
-            {currentView === "list" ? (
+            {showMap ? (
+              <AllHikesMap data={data} navigation={navigation} />
+            ) : (
               <ScrollView
                 className="flex-1"
                 showsVerticalScrollIndicator={false}
               >
                 <PastHikeList data={data} navigation={navigation} />
               </ScrollView>
-            ) : (
-              <AllHikesMap data={data} navigation={navigation} />
             )}
           </View>
         </>
